0eaa516f6e5572e4aa5314ca830e806283ed0eef
yekecheng <yekecheng@bytedance.com>
2021-04-15 15:32:50 +0800

    add: change main function

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 from Game_component import *
 
 def main():
-    game = Glutonous_Snake(5, 5)
+    game = Glutonous_Snake(20, 20)
     game.start()
 
 
 if __name__ == '__main__':
-    # main()
-    engine = Render_engine('terminal', None)
-    board = board(5, 5, snake_init_coordinates = [3, 1], fruit_init_coordinates = [0, 2])
-    board.Snake_init_from_lst([[1, 2], [2, 2], [3, 2], [4, 2], [4, 1]])
-    board.Update_board()
-    engine.render_terminal(board)
-    board.Snake_move("up")
-    board.Update_board()
-    engine.render_terminal(board)
+    main()
